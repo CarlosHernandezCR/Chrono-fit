1c50fd4ca7c6c314a1fe59871ffb1d51309c43d4
Carlos Raul Hernández Campos <c.hcampos@outlook.es>
2025-05-25 22:41:26 +0200

    Arreglo de versiones

--- Chrono_FitApp/app/build.gradle.kts
+++ Chrono_FitApp/app/build.gradle.kts
@@ -1,9 +1,9 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
-    alias(libs.plugins.kotlin.compose)
     alias(libs.plugins.daggerHilt)
     alias(libs.plugins.ksp)
+    alias(libs.plugins.kotlin.compose)
 }
 
 android {
