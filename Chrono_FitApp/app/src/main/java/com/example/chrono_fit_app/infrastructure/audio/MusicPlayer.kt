6e52535206e09658e2f46155c472a43568b9b2db
Carlos Raul Hernández Campos <c.hcampos@outlook.es>
2025-06-16 20:44:39 +0200

    Arreglo del sonido para que no pause la musica que este puesta en el movil

--- Chrono_FitApp/app/src/main/java/com/example/chrono_fit_app/infrastructure/audio/MusicPlayer.kt
+++ Chrono_FitApp/app/src/main/java/com/example/chrono_fit_app/infrastructure/audio/MusicPlayer.kt
@@ -1,29 +1,39 @@
 package com.example.chrono_fit_app.infrastructure.audio
 
 import android.content.Context
+import android.media.AudioAttributes
 import android.media.SoundPool
 import com.example.chrono_fit_app.R
 import com.example.chrono_fit_app.domain.audio.MusicPlayerI
 
-
 class MusicPlayer(context: Context) : MusicPlayerI {
-    private val soundPool = SoundPool.Builder().setMaxStreams(1).build()
-    private val pitido: Int = soundPool.load(context, R.raw.beep, 1)
-    private val pitidoFinal: Int = soundPool.load(context, R.raw.beep_end, 1)
 
-    fun tocarPitido() {
-        soundPool.play(pitido, 1f, 1f, 0, 0, 1f)
-    }
+    private val soundPool: SoundPool
 
-    fun tocarPitidoFinal() {
-        soundPool.play(pitidoFinal, 1f, 1f, 0, 0, 1f)
+    private val pitido: Int
+    private val pitidoFinal: Int
+
+    init {
+        val audioAttributes = AudioAttributes.Builder()
+            .setUsage(AudioAttributes.USAGE_NOTIFICATION)
+            .setContentType(AudioAttributes.CONTENT_TYPE_SONIFICATION)
+            .build()
+
+        soundPool = SoundPool.Builder()
+            .setAudioAttributes(audioAttributes)
+            .setMaxStreams(1)
+            .build()
+
+        pitido = soundPool.load(context, R.raw.beep, 1)
+        pitidoFinal = soundPool.load(context, R.raw.beep_end, 1)
     }
 
     override fun playPitido() {
-        tocarPitido()
+        soundPool.play(pitido, 1f, 1f, 0, 0, 1f)
     }
+
     override fun playPitidoFinal() {
-        tocarPitidoFinal()
+        soundPool.play(pitidoFinal, 1f, 1f, 0, 0, 1f)
     }
 
     override fun release() {
